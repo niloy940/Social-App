5d85999eb45931d13b4c32f658f1029333ada4dd
Niloy Quazi <niloy940@gmail.com>
2021-11-07 14:55:10 +0600

    backend setup and connecting to database

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+const CONNECTION_URL = 'mongodb+srv://niloy:niloy940@cluster0.dezrp.mongodb.net/memories_db?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+	.catch((error) => console.log(error.message));
