46c1b5c9b806075b72bb5304faed06568a78fab5
Niloy Quazi <niloy940@gmail.com>
2021-11-07 15:52:05 +0600

    backend folder structure

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
-
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://niloy:niloy940@cluster0.dezrp.mongodb.net/memories_db?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
